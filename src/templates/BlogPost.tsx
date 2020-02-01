089649e523932023a5deef0ee3f9de5eb9310fd5
Proudust <proudust@gmail.com>
2020-02-01 17:15:22 +0900

    :fire: Remove unnecessary code

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -51,14 +51,8 @@ export default BlogPost;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
-      excerpt(pruneLength: 160)
       html
       frontmatter {
         title
