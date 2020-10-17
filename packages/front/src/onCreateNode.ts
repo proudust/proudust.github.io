97e9bcb4df0365377522000ba5ac47862275a71e
Proudust <proudust@gmail.com>
2020-10-17 22:20:21 +0900

    :recycle: Split onCreateNode functions

--- packages/front/src/onCreateNode.ts
+++ packages/front/src/onCreateNode.ts
@@ -2,9 +2,31 @@ import { basename, dirname, posix, relative, resolve } from 'path';
 import { createFilePath } from 'gatsby-source-filesystem';
 import simpleGit from 'simple-git';
 
-import type { GatsbyNode } from 'gatsby';
+import type { CreateNodeArgs, GatsbyNode } from 'gatsby';
 
-function getSlug(path: string): string {
+interface MarkdownRemark {
+  readonly fileAbsolutePath: string;
+}
+
+function isMarkdownRemarkNode(x: CreateNodeArgs): x is CreateNodeArgs<MarkdownRemark> {
+  return x.node.internal.type === 'MarkdownRemark';
+}
+
+function appendSlug(args: CreateNodeArgs): void {
+  if (!isMarkdownRemarkNode(args)) return;
+
+  const { node, actions } = args;
+  const absolutePath = node.fileAbsolutePath;
+  if (!absolutePath) return;
+  const isInside = absolutePath.endsWith('index.md');
+  const slug = isInside ? createFilePath(args) : getSlugFromZennArticles(absolutePath);
+  actions.createNodeField({ name: 'slug', node, value: slug });
+
+  const source = isInside ? 'inside' : 'zenn';
+  actions.createNodeField({ name: 'source', node, value: source });
+}
+
+function getSlugFromZennArticles(path: string): string {
   const fileName = basename(path);
   const slug = fileName
     .substring(0, fileName.length - 3)
@@ -12,32 +34,32 @@ function getSlug(path: string): string {
   return posix.join('/', slug, '/');
 }
 
-export const onCreateNode: GatsbyNode['onCreateNode'] = async ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
+async function appendGitInfo(args: CreateNodeArgs): Promise<void> {
+  if (!isMarkdownRemarkNode(args)) return;
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const absolutePath = node.fileAbsolutePath as string;
-    const isInside = absolutePath.endsWith('index.md');
-    const slug = isInside ? createFilePath({ node, getNode }) : getSlug(absolutePath);
-    createNodeField({ name: `slug`, node, value: slug });
-
-    const source = isInside ? 'inside' : 'zenn';
-    createNodeField({ name: `source`, node, value: source });
-
-    let dirPath = dirname(absolutePath);
-    while (true) {
-      const git = simpleGit(dirPath);
-      const relativePath = relative(dirPath, absolutePath);
-      const commits = await git.log({ file: relativePath }).catch(() => undefined);
-      if (!commits) {
-        dirPath = resolve(dirPath, '..');
-        continue;
-      }
-      const createAt = commits.all[commits.total - 1].date;
-      const updateAt = commits.latest.date;
-      createNodeField({ name: `createat`, node, value: createAt });
-      createNodeField({ name: `updateat`, node, value: updateAt });
-      break;
+  const { node, actions } = args;
+  const absolutePath = node.fileAbsolutePath;
+  if (!absolutePath) return;
+  let dirPath = dirname(absolutePath);
+  while (true) {
+    const git = simpleGit(dirPath);
+    const relativePath = relative(dirPath, absolutePath);
+    const commits = await git.log({ file: relativePath }).catch(() => undefined);
+    if (!commits) {
+      dirPath = resolve(dirPath, '..');
+      continue;
     }
+    const createAt = commits.all[commits.total - 1].date;
+    const updateAt = commits.latest.date;
+    actions.createNodeField({ name: `createat`, node, value: createAt });
+    actions.createNodeField({ name: `updateat`, node, value: updateAt });
+    break;
   }
+}
+
+export const onCreateNode: GatsbyNode['onCreateNode'] = async args => {
+  const promises: Promise<void>[] = [];
+  promises.push(appendGitInfo(args));
+  appendSlug(args);
+  return Promise.all(promises);
 };
