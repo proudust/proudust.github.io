1d2d38d3c45b226921140bcb0b129b2de3f99f95
Proudust <proudust@gmail.com>
2020-10-18 16:28:34 +0900

    :zap: Change ZennIcon format from png to svg

--- packages/front/src/components/ZennIcon.tsx
+++ packages/front/src/components/ZennIcon.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
 
 interface IconProps {
   children?: never;
 }
 
-export const ZennIcon: React.FC<IconProps> = () => {
-  const { file } = useStaticQuery<GatsbyTypes.ZennIconQuery>(query);
-  const fixed = file?.childImageSharp?.fixed;
-  return <Img alt="zenn" fixed={fixed!} />;
-};
-
-export const query = graphql`
-  query ZennIcon {
-    file(relativePath: { eq: "zenn.png" }) {
-      childImageSharp {
-        fixed(width: 24, height: 24) {
-          base64
-          width
-          height
-          src
-          srcSet
-        }
-      }
-    }
-  }
-`;
+// reference: https://zenn.dev
+export const ZennIcon: React.FC<IconProps> = () => (
+  <svg viewBox="0 0 88.3 88.3" width="24" height="24">
+    <path d="M2.4 83.3h17c.9 0 1.7-.5 2.2-1.2L68.4 5.2c.6-1-.1-2.2-1.3-2.2H51c-.8 0-1.5.4-1.9 1.1L1.6 81.9c-.3.6.1 1.4.8 1.4zM61 82.1l22.1-35.5c.7-1.1-.1-2.5-1.4-2.5h-16c-.6 0-1.2.3-1.5.8L41.5 81.2c-.6.9.1 2.1 1.2 2.1H59c.8 0 1.6-.4 2-1.2z" />
+  </svg>
+);
