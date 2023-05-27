13390aa0d847052e677f3663bff59b09d1dbac26
Proudust <proudust@gmail.com>
2023-05-27 23:03:25 +0900

    :wrench: Convert .eslintrc.yml to .eslintrc.cjs

---
+++ packages/front/.eslintrc.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'react-app',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+  },
+  plugins: ['react'],
+  processor: '@graphql-eslint/graphql',
+  rules: {
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      { allowExpressions: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: ['*.graphql'],
+      parser: '@graphql-eslint/eslint-plugin',
+      plugins: ['@graphql-eslint'],
+      rules: {
+        '@graphql-eslint/no-anonymous-operations': 'error',
+        '@graphql-eslint/naming-convention': [
+          'error',
+          {
+            OperationDefinition: {
+              style: 'PascalCase',
+              forbiddenPrefixes: ['Query', 'Mutation', 'Subscription', 'Get'],
+              forbiddenSuffixes: ['Query', 'Mutation', 'Subscription'],
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
